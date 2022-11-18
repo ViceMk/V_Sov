7dd159c1ba4693a225e1b64b560a34af484169b8
Vice Mkansi <51099795+ViceMk@users.noreply.github.com>
2022-11-18 12:36:01 +0200

    Add files via upload

---
+++ PortFolio-main/src/App.js
@@ -0,0 +1,100 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-image">
+    <div className="App">
+      
+      
+      <ul>
+  <li><a class="active" href="#home">V I C E.DEV</a></li>
+ 
+</ul>
+       
+      {/* <div className="container-fluid row justify-content-center">
+        <div className="col-sm-7">
+          <div classNam="card  justify-content-center">
+          <div className="card-body">
+           
+            <br/>
+           </div>
+        </div>
+        </div>
+      </div> */}
+      <div className='header'>
+      <div className="img"></div>
+       <h1 className="Intro">Hi SoveTech,</h1>  &nbsp;	&nbsp; <h1 className="Names">—My name is Vice Mkansi—</h1> <br></br> &nbsp;
+       <p className='p1'>MySQL | .Net core MVC | .NET Framework | C# | HTML | Angular10 | Bootstrap  | CSS | TypeScript | Angular10+ | MVC Architecture | UI
+DESIGN | JavaScript</p>
+       </div>  
+            {/* <hr className=""/> */}
+{/* New Cards */}
+
+
+  <div className="column">
+    <div className="card1">
+      <h3>About Me</h3>
+      <p>   I am a diligent, confident, and results driven Informatics(Information Systems) graduate from University of Pretoria. 
+             I am currently a junior self-taught sofware developer with no professional work experience. I have the ability to
+             efficiently prioritize and multitask projects. I am versatile and solutions-driven with broadranging experience in improving business and operational processes by leveraging
+             natural analytical abilities. I always do my best to achieve the highest level of quality
+             results through my meticulous and discipline, I believe my skill-set can add value to your
+             company. I enjoy working with other people, especially as a team and I am also
+             comfortable working on my own. I fulfill strong organizational leadership</p>
+      
+    </div>
+  </div>
+
+  <div className="column">
+    <div className="card1">
+      <h3> Software Development</h3>
+      <p> Currently I am a on a junior level full stack developer with an emphasis on: JavaScript, 
+              Angular10+, React.js, Java (programming language), node.js, express.js, 
+              Ionic,  MySQL, SQL, postman, REST API, JSON, AJAX, bcrypt, JWT, dotenv, CSS,
+              Bootstrap, HTML
+              As a frontend developer, I am quite comfortable with the latest trends and standards in Frontend 
+              development. I am a fast learner and a team-player. You may count on me being an asset to your project. 
+              If the project requires the knowledge of other frameworks, currently not part of my stack, I am willing 
+              to quickly learn them.
+              As a backend developer, I am quite comfortable with the latest trends and standards in backend 
+              development. I am always striving to increase my knowledge and keep myself updated and to keep my 
+              skills relevant in the world of software development. I am willing to quickly learn any technology 
+              that may be required for any project. I have real world experience from my final year project working on a bigger project</p>
+      
+    </div>
+  </div>
+   {/* Why do I want to work at SoveTech */}
+  <div className="column">
+    <div className="card1">
+      <h3>Why SovTech</h3>
+      <p>I would like to work for SovTech, based on your history you seem
+             to be a very focused company with big plans for the future, I see myself advancing and growing professionally with your company.
+             I am very much interested in gaining hands-on knowledge because it will ultimately complement my 
+             theoretical knowledge and help me improve in my skills, I believe my skillset can
+             add value to your company. Through my project work experience and other professional activities I have strengthened
+            my interpersonal and presentation skills. I aver that with my cumulative experience which I gained
+            from diverse sources, I’m quite confident that I will be present on this graduate program and taking with full responsibility</p>
+    
+    </div>
+  </div>
+  
+ 
+
+
+  
+  
+
+
+
+
+
+
+
+
+    </div>
+    </div>
+  );
+}
+
+export default App;
